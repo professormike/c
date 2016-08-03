c144dbc4814d37ccd1f7b23df7af49c2308af1ca
Michael Burrell <michael.burrell@sheridancollege.ca>
2016-08-03 16:21:36 -0400

    Implemented reachability

--- week12/graph.c
+++ week12/graph.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 static char *nodes[] = { "A", "B", "C", "D", "E", "F", "G", "H" };
-#define N	(sizeof nodes / sizeof *nodes)
+#define N	8
 static int matrix_rows[][N] = {
 	{ 0, 5, 3, -1, -1, -1, -1, -1 },	// nodes reachable from A
 	{ -1, 0, -1, -1, -1, -1, -1, -1 },	// nodes reachable from B
@@ -13,6 +14,33 @@ static int matrix_rows[][N] = {
 	{ -1, -1, -1, -1, -1, -1, 6, 0 }	// nodes reachable from H
 };
 
+void
+reachable(int current_node, char **nodes, int **edges, bool *visited)
+{
+	if (visited[current_node]) {
+		// do nothing
+	} else {
+		printf("visited %s\n", nodes[current_node]);
+		visited[current_node] = true;
+		for (size_t i = 0; i < N; i++) {
+			if (edges[current_node][i] > 0) {	// found a node to visit
+				reachable(i, nodes, edges, visited);
+			}
+		}
+	}
+}
+
+bool
+can_reach(int from, int to, char **nodes, int **edges)
+{
+	bool visited[N];
+	for (size_t i = 0; i < N; i++) {
+		visited[i] = false;
+	}
+	reachable(from, nodes, edges, visited);
+	return visited[to];
+}
+
 int
 main(void)
 {
@@ -20,5 +48,7 @@ main(void)
 	for (size_t i = 0; i < N; i++) {
 		edges[i] = matrix_rows[i];
 	}
+	printf("A to D? %d\n", can_reach(0, 3, nodes, edges));
+	printf("A to G? %d\n", can_reach(0, 6, nodes, edges));
 	return 0;
 }
